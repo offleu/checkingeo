5e90cd01e24182e209b208fa991d3e9c214af0ae
Leonardo <leonardo.rlima09@gmail.com>
2026-05-30 09:28:29 -0300

    PRIMEIRO COMMIT

---
+++ src/main/java/com/inovaa/checkingeo/CheckingeoApplication.java
@@ -0,0 +1,13 @@
+package com.inovaa.checkingeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckingeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inovaa/checkingeo/CheckingeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovaa.checkingeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
